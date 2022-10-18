5128bf6750a1329315e7067a9c79e2e5ed09083e
KrakAtmin <97604209+KrakAtmin@users.noreply.github.com>
2022-10-18 13:14:15 +0200

    jednak działa

--- adrescompare.cpp
+++ adrescompare.cpp
@@ -6,19 +6,19 @@ bool CzyPoprawnyAdres(string adres)
     {
         int dl = adres.length();
 
-        int i = 0;
+        int i = 0; //sprawdzanie index znaku @
         while (adres[i]!='@' && i<dl-1) i++;
         if (adres[i]!='@' || i<2) return false;
 
-        int j = dl-1;
+        int j = dl-1; //sprawdzanie index znaku @ od tylu i porównywanie do i
         while (adres[j]!='@') j--;
         if (i!=j) return false;
 
-        int k = dl-1;
+        int k = dl-1; //sprawdzanie index kropki od tyłu
         while (adres[k]!='.' && k>0) k--;
         if (adres[k]!='.'||!(k==dl-3||k==dl-4)) return false;
 
-        if (k-i<=1) return false;
+        if (k-i<=1) return false; //sprawdzanie czy kropka jest po @
 
         return true;
     }
