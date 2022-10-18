19bad0b5f5c7eb84a0c825dc01c0d8bbff09b763
KrakAtmin <97604209+KrakAtmin@users.noreply.github.com>
2022-10-18 13:10:46 +0200

    nie dziala

---
+++ adrescompare.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+bool CzyPoprawnyAdres(string adres)
+    {
+        int dl = adres.length();
+
+        int i = 0;
+        while (adres[i]!='@' && i<dl-1) i++;
+        if (adres[i]!='@' || i<2) return false;
+
+        int j = dl-1;
+        while (adres[j]!='@') j--;
+        if (i!=j) return false;
+
+        int k = dl-1;
+        while (adres[k]!='.' && k>0) k--;
+        if (adres[k]!='.'||!(k==dl-3||k==dl-4)) return false;
+
+        if (k-i<=1) return false;
+
+        return true;
+    }
+
+int main()
+{
+    string adres;
+    cin >> adres;
+    cout << CzyPoprawnyAdres(adres);
+    if (CzyPoprawnyAdres(adres) == true) {
+        cout << "Adres poprawny";
+    } else {
+        cout << "Adres niepoprawny";
+    }
+}
